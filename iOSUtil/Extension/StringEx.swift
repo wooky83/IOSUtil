206ea96b104d2539582a73e1e976bf9d53a32163
1002659 <baw0803@sk.com>
2021-03-22 11:11:48 +0900

    [Dev] revise attributed String

--- iOSUtil/Extension/StringEx.swift
+++ iOSUtil/Extension/StringEx.swift
@@ -12,6 +12,7 @@ extension String {
     
     //아주 많이 느릴때가 있음...
     //https://stackoverflow.com/questions/31852655/very-slow-html-rendering-in-nsattributedstring
+    //https://stackoverflow.com/questions/21166752/why-does-the-initial-call-to-nsattributedstring-with-an-html-string-take-over-10
     //http://www.robpeck.com/2015/04/nshtmltextdocumenttype-is-slow/#.XIDdcVMzZTY
     func attributedStringFromHtml(familyName: String = "Apple SD Gothic Neo", fontColor: String = "#ffffff", fontSize: CGFloat) -> NSAttributedString {
         let html = "<font face=\"\(familyName)\" color=\"\(fontColor)\"><span style= \"font-size:\(Int(fontSize))\">\(self)</span></font>"
