90c2134347e1c1afdeddd820748860d4840c8aa2
Kwon Sung Wook[ 권성욱 ] <baw0803@linecorp.com>
2023-06-27 16:58:31 +0900

    Refactoring With SPM

---
+++ Sources/SWUtil/SWUtil.swift
@@ -0,0 +1,6 @@
+public struct SWUtil {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SWUtilTests/SWUtilTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SWUtil
+
+final class SWUtilTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SWUtil().text, "Hello, World!")
+    }
+}
