239b549111f4a6228f9c4b786bc9f5eb553614d4
Kwon Sung Wook[ 권성욱 ] <baw0803@linecorp.com>
2023-09-11 18:18:10 +0900

    Add Then

--- Tests/SWUtilTests/SWProtocolTests.swift
+++ Tests/SWUtilTests/SWProtocolTests.swift
@@ -37,4 +37,35 @@ final class SWProtocolTests: XCTestCase {
         XCTAssertNotNil(myPerson)
         XCTAssertEqual(myPerson?.day, .monday)
     }
+
+    func testThenThen() throws {
+        class MyClass: NSObject {
+            var value: String?
+        }
+        let object = MyClass().then {
+            $0.value = "hello"
+        }
+        XCTAssertEqual("hello", object.value)
+    }
+
+    func testThenWith() throws {
+        let rect = CGRect.zero
+        let frame = rect.with {
+            $0.origin.x = 10
+            $0.size.width = 100
+        }
+        XCTAssertEqual(rect, CGRect.zero)
+        XCTAssertEqual(frame, CGRect(x: 10, y: 0, width: 100, height: 0))
+    }
+
+    func testThenDo() throws {
+        class MyClass: NSObject {
+            var value: String?
+        }
+        let object = MyClass()
+        object.do {
+            $0.value = "hello"
+        }
+        XCTAssertEqual("hello", object.value)
+    }
 }
