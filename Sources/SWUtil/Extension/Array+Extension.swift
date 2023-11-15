67c1d54cd2254ece0d4c53b4662ede7ec7d3663c
wooky <baw0803@nate.com>
2023-11-15 10:20:12 +0900

    Refactoring (#16)
    * Refactoring
    
    * Add
    
    ---------
    
    Co-authored-by: Kwon Sung Wook[ 권성욱 ] <baw0803@linecorp.com>

--- Sources/SWUtil/Extension/Array+Extension.swift
+++ Sources/SWUtil/Extension/Array+Extension.swift
@@ -1,25 +1,22 @@
 import Foundation
 
-extension Array: SWCompatible { }
-
-public extension SW where Base: RangeReplaceableCollection {
-    subscript(safe index: Int) -> Base.Element? {
-        let index = base.index(base.startIndex, offsetBy: index)
-        return base.indices.contains(index) ? base[index] : nil
+extension Collection {
+    subscript(safe index: Int) -> Element? {
+        let index = self.index(startIndex, offsetBy: index)
+        return indices.contains(index) ? self[index] : nil
     }
-}
 
-public extension SW where Base: Collection {
-    subscript(circular index: Int) -> Base.Element {
+    subscript(circular index: Int) -> Element {
         index >= 0 ?
-            base[base.index(base.startIndex, offsetBy:(index % base.count))] :
-            base[base.index(base.startIndex, offsetBy:(base.count - abs(index) % base.count))]
+            self[self.index(startIndex, offsetBy:(index % count))] :
+            self[self.index(startIndex, offsetBy:(count - abs(index) % count))]
     }
+
 }
 
-public extension SW where Base: RangeReplaceableCollection, Base.Element: Hashable {
-    var arrayDeduplicated: Base {
-        var dict: [Base.Element: Bool] = [:]
-        return base.filter { dict.updateValue(true, forKey: $0) == nil }
+extension Collection where Element: Hashable {
+    var arrayDeduplicated: [Self.Element] {
+        var dict: [Element: Bool] = [:]
+        return filter { dict.updateValue(true, forKey: $0) == nil }
     }
 }
--- Sources/SWUtil/Extension/Data+Extension.swift
+++ Sources/SWUtil/Extension/Data+Extension.swift
@@ -1,10 +1,9 @@
 import Foundation
 
-extension Data: SWCompatible {}
-extension SW where Base == Data {
+extension Data {
     var base64URLEncodedString: String { base64URLEncodedString() }
     func base64URLEncodedString(with option: Data.Base64EncodingOptions = []) -> String {
-        base.base64EncodedString(options: option)
+        self.base64EncodedString(options: option)
             .replacingOccurrences(of: "+", with: "-")
             .replacingOccurrences(of: "/", with: "_")
             .replacingOccurrences(of: "=", with: "")
