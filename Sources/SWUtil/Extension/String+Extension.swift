2296b6fe056877b716d57aa645ddbac2e13fb992
Kwon Sung Wook[ 권성욱 ] <baw0803@linecorp.com>
2023-11-17 17:02:55 +0900

    Refactoring

--- Sources/SWUtil/Extension/String+Extension.swift
+++ Sources/SWUtil/Extension/String+Extension.swift
@@ -1,5 +1,5 @@
 #if canImport(UIKit)
-import UIKit
+    import UIKit
 #endif
 import Foundation
 
@@ -20,7 +20,8 @@ public extension String {
     var prettyString: String {
         guard let object = json,
               let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
-              let prettyPrintedString = String(data: data, encoding: .utf8) else {
+              let prettyPrintedString = String(data: data, encoding: .utf8)
+        else {
             return self
         }
         return prettyPrintedString
@@ -28,7 +29,8 @@ public extension String {
 
     var json: [String: Any]? {
         guard let data = data(using: .utf8),
-              let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+              let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any]
+        else {
             return nil
         }
 
@@ -73,54 +75,52 @@ public extension String {
     subscript(_ range: ClosedRange<Int>) -> String {
         let start = index(startIndex, offsetBy: range.lowerBound)
         let end = index(startIndex, offsetBy: range.upperBound)
-        return String(self[start...end])
+        return String(self[start ... end])
     }
 
     subscript(_ range: Range<Int>) -> String {
         let start = index(startIndex, offsetBy: range.lowerBound)
         let end = index(startIndex, offsetBy: range.upperBound)
-        return String(self[start..<end])
+        return String(self[start ..< end])
     }
 
     subscript(_ range: PartialRangeThrough<Int>) -> String {
         let end = index(startIndex, offsetBy: range.upperBound)
-        return String(self[startIndex...end])
+        return String(self[startIndex ... end])
     }
 
     subscript(_ range: PartialRangeFrom<Int>) -> String {
         let start = index(startIndex, offsetBy: range.lowerBound)
-        return String(self[start..<endIndex])
+        return String(self[start ..< endIndex])
     }
-    
-#if canImport(UIKit)
-    //아주 많이 느릴때가 있음...
-    //https://stackoverflow.com/questions/31852655/very-slow-html-rendering-in-nsattributedstring
-    //https://stackoverflow.com/questions/21166752/why-does-the-initial-call-to-nsattributedstring-with-an-html-string-take-over-10
-    //http://www.robpeck.com/2015/04/nshtmltextdocumenttype-is-slow/#.XIDdcVMzZTY
-    func attributedStringFromHtml(familyName: String = "Apple SD Gothic Neo", fontColor: String = "#ffffff", fontSize: CGFloat) -> NSAttributedString {
-        let html = "<font face=\"\(familyName)\" color=\"\(fontColor)\"><span style= \"font-size:\(Int(fontSize))\">\(self)</span></font>"
-        guard let data = html.data(using: .utf8) else { return NSAttributedString() }
-        do {
-            return try NSAttributedString(data: data, options: [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html, NSAttributedString.DocumentReadingOptionKey.characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
-        } catch{
-            return NSAttributedString()
+
+    #if canImport(UIKit)
+        // 아주 많이 느릴때가 있음...
+        // https://stackoverflow.com/questions/31852655/very-slow-html-rendering-in-nsattributedstring
+        // https://stackoverflow.com/questions/21166752/why-does-the-initial-call-to-nsattributedstring-with-an-html-string-take-over-10
+        // http://www.robpeck.com/2015/04/nshtmltextdocumenttype-is-slow/#.XIDdcVMzZTY
+        func attributedStringFromHtml(familyName: String = "Apple SD Gothic Neo", fontColor: String = "#ffffff", fontSize: CGFloat) -> NSAttributedString {
+            let html = "<font face=\"\(familyName)\" color=\"\(fontColor)\"><span style= \"font-size:\(Int(fontSize))\">\(self)</span></font>"
+            guard let data = html.data(using: .utf8) else { return NSAttributedString() }
+            do {
+                return try NSAttributedString(data: data, options: [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html, NSAttributedString.DocumentReadingOptionKey.characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+            } catch {
+                return NSAttributedString()
+            }
         }
-    }
 
-    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
-        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
-        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+            let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+            let boundingBox = boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
 
-        return ceil(boundingBox.height)
-    }
+            return ceil(boundingBox.height)
+        }
 
-    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
-        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
-        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+            let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+            let boundingBox = boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
 
-        return ceil(boundingBox.width)
-    }
-#endif
-
-
+            return ceil(boundingBox.width)
+        }
+    #endif
 }
